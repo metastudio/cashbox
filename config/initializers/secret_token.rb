5056e601cd4a6366c4b0328732a1465cb85b85c8
Ildar Shaynurov <shaynurov@gmail.com>
2014-02-07 16:06:09 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cashbox::Application.config.secret_key_base = '9a79f9794a68cb8a1cdbbb3a1e9b73f82d2e225eef44f8b47bac7f056826eb94b12155c652a94a1f3e2e65d452641fb3768df54f71c05f9d2b6b223986a02dc1'
