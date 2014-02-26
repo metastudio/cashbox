4593edf5623dfa00542dc53eb792996bddcd54f1
Ildar Shaynurov <shaynurov@gmail.com>
2014-02-26 12:37:43 +0400

    Fix configuration for specs

--- spec/support/headless.rb
+++ spec/support/headless.rb
@@ -1,5 +1,19 @@
+def start_headless
+  # based on http://stackoverflow.com/questions/15823177/continuous-integration-running-parallel-tests-suites-that-require-xvfb
+  if (!$headless_started)
+    # allow display autopick (by default)
+    # allow each headless to destroy_at_exit (by default)
+    # allow each process to have their own headless by setting reuse: false
+    headless_server = Headless.new(:reuse => false)
+    headless_server.start
+
+    $headless_started = true
+    puts "Process[#{Process.pid}] started headless server display: #{headless_server.display}"
+  end
+end
+
 RSpec.configure do |config|
   config.before(:each, js: true) do
-    Headless.new.start
+    start_headless
   end
 end
