b259f993f895212e5813e9aabd58691a9c3c8f60
Ildar Shaynurov <shaynurov@gmail.com>
2014-02-26 12:48:03 +0400

    Spec: refactor matcher for flash messages

--- spec/support/matchers/have_flash_message_matcher.rb
+++ spec/support/matchers/have_flash_message_matcher.rb
@@ -1,22 +1,24 @@
 RSpec::Matchers.define :have_flash_message do |message|
+  FLASH_MESSAGE_SELECTOR = '.flash-messages'
+
   match_for_should do |page|
-    page.within('.flash-messages') do
+    page.within(FLASH_MESSAGE_SELECTOR) do
       page.has_content?(message)
     end
   end
 
   match_for_should_not do |page|
-    page.within('.flash-messages') do
-      page.has__nocontent?(message)
+    page.within(FLASH_MESSAGE_SELECTOR) do
+      page.has_no_content?(message)
     end
   end
 
   failure_message_for_should do |page|
-    %Q{expected to have flash message "#{message}" in "#{page.text}"}
+    %Q{expected to have flash message "#{message}" in "#{page.find(FLASH_MESSAGE_SELECTOR).try(:text)}"}
   end
 
   failure_message_for_should_not do |page|
-    %Q{expected to not have flash message "#{message}" in "#{page.text}"}
+    %Q{expected to not have flash message "#{message}" in "#{page.find(FLASH_MESSAGE_SELECTOR).try(:text)}"}
   end
 
   description do
