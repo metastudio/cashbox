1fee070ae5303b7abb89a4bfbbeaa05f31851516
Ildar Shaynurov <shaynurov@gmail.com>
2014-03-13 18:20:34 +0400

    Fix and refactor

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,22 +1,19 @@
 class BankAccount < ActiveRecord::Base
-  CURRENCIES = %w(USD RUB)
+  CURRENCY_USD = 'USD'
+  CURRENCY_RUB = 'RUB'
+  CURRENCIES = [CURRENCY_USD, CURRENCY_RUB]
 
   belongs_to :organization, inverse_of: :bank_account
-  has_many :transactions
+  has_many :transactions, dependent: :destroy, inverse_of: :bank_account
 
-  monetize :balance_cents, with_model_currency: :balance_currency, allow_nil: true
-  
+  monetize :balance_cents, with_model_currency: :balance_currency
+
   validates :name,             presence: true
   validates :balance,          presence: true
-  validates :balance_currency, presence: true
-  validates :balance_currency, inclusion: { in: CURRENCIES, message: "%{value} is not a valid currency" }
+  validates :balance_currency, presence: true, inclusion: { in: CURRENCIES, message: "%{value} is not a valid currency" }
 
-  def self.total_balance_in_dollars
-    BankAccount.where(balance_currency: 'USD').sum(&:balance)
-  end
-
-  def self.total_balance_in_rubles
-    BankAccount.where(balance_currency: 'RUB').sum(&:balance)
+  def self.total_balance_cents(currency)
+    where(balance_currency: currency).sum(:balance_cents)
   end
 
   def recalculate_amount
