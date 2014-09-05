187f00ad68d27f3c8e3550bfcd8c503a602152d8
Peter <psmir777@gmail.com>
2014-09-05 12:07:32 +0400

    switch organizations

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Cashbox::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#show'
 
-  resources :organizations
+  resources :organizations do
+    member do
+      put :switch
+    end
+  end
   resources :bank_accounts, except: [:show, :index]
   resources :categories
   resources :transactions,  only: [:create, :edit, :update, :destroy]
