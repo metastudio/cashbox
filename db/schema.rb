ecde611f5734160d12af510a248a6f718c9e8a52
gataniel <gataniel@gmail.com>
2015-01-21 14:29:02 +0400

    Add migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140908161521) do
+ActiveRecord::Schema.define(version: 20150121101954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20140908161521) do
     t.datetime "updated_at"
     t.string   "comment"
     t.string   "transaction_type"
+    t.datetime "date"
   end
 
   add_index "transactions", ["bank_account_id"], name: "index_transactions_on_bank_account_id", using: :btree
