6be10ef11a46db3222e7031e2a5ea7785100ebf8
gataniel <gataniel@gmail.com>
2015-01-21 14:04:42 +0400

    Fix migration method on fixing owners

--- db/migrate/20140908161521_remove_owner_id_from_organization.rb
+++ db/migrate/20140908161521_remove_owner_id_from_organization.rb
@@ -1,6 +1,8 @@
 class RemoveOwnerIdFromOrganization < ActiveRecord::Migration
   Organization.where.not(owner_id: nil).find_each do |org|
-    org.members.create(user_id: org.owner_id, role: 'owner')
+    unless org.members.find_by(user_id: org.owner_id, role: 'owner')
+      org.members.create(user_id: org.owner_id, role: 'owner')
+    end
   end
 
   def change
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904150019) do
+ActiveRecord::Schema.define(version: 20140908161521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 20140904150019) do
     t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "owner_id"
   end
 
   create_table "profiles", force: true do |t|
