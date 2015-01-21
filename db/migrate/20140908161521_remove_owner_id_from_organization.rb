bb714aca15b4e807e45f81b875d0ea3ae56d2f47
gataniel <gataniel@gmail.com>
2015-01-21 13:59:57 +0400

    Fix migration and association

--- db/migrate/20140908161521_remove_owner_id_from_organization.rb
+++ db/migrate/20140908161521_remove_owner_id_from_organization.rb
@@ -1,4 +1,8 @@
 class RemoveOwnerIdFromOrganization < ActiveRecord::Migration
+  Organization.where.not(owner_id: nil).find_each do |org|
+    org.members.create(user_id: org.owner_id, role: 'owner')
+  end
+
   def change
     remove_column :organizations, :owner_id, :integer
   end
