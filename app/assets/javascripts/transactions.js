a01ebbc8b5cca70dceaeb278aad8495e1212c608
gataniel <gataniel@gmail.com>
2015-01-28 12:20:05 +0400

    Fix editing transaction when switch to next page

--- app/assets/javascripts/transactions.js
+++ app/assets/javascripts/transactions.js
@@ -1,9 +1,9 @@
 $(function () {
-  $(".transactions").on("click", ".transaction[data-edit-url]", function(el) {
-    el.preventDefault();
+  $(document).on('click', '.transaction[data-edit-url]', function(e) {
+    e.preventDefault();
     $.ajax({
       url: $(this).data("edit-url"),
       dataType: "script"
     });
-  })
+  });
 });
