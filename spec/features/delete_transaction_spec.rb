222b2da817aa9f55ab52d6f36cbc4c1c78be51a1
gataniel <gataniel@gmail.com>
2015-02-02 11:57:36 +0400

    Update deleting test

--- spec/features/delete_transaction_spec.rb
+++ spec/features/delete_transaction_spec.rb
@@ -8,40 +8,64 @@ describe 'delete transaction', js: true do
   let!(:category)     { create :category, organization: organization }
   let!(:account)      { create :bank_account, organization: organization}
 
-  let!(:transactions) { create_list :transaction, 25, bank_account: account,
-    category: category }
-  let(:first_transaction) { transactions.last }
-  let(:last_transaction)  { transactions.first }
-
   before do
     sign_in user
-    visit root_path
   end
 
   subject{ page }
 
-  context "first page" do
+  context "within pagination" do
+    let!(:transactions) { create_list :transaction, 25, bank_account: account,
+      category: category }
+    let(:first_transaction) { transactions.last }
+    let(:last_transaction)  { transactions.first }
+
     before do
-      find("#transaction_#{first_transaction.id}",
-        text: humanized_money_with_symbol(first_transaction.amount)).click
-      page.has_css?('simple_form edit_transaction')
+      visit root_path
     end
 
-    it "shows delete form on row click" do
-      expect(subject).to have_link("Remove", href: transaction_path(first_transaction))
+    context "first page" do
+      before do
+        find("#transaction_#{first_transaction.id}",
+          text: humanized_money_with_symbol(first_transaction.amount)).click
+        page.has_css?('simple_form edit_transaction')
+      end
+
+      it "shows delete form on row click" do
+        expect(subject).to have_link("Remove", href: transaction_path(first_transaction))
+      end
+    end
+
+    context "next page" do
+      before do
+        click_on 'Last'
+        find("#transaction_#{last_transaction.id}",
+          text: humanized_money_with_symbol(last_transaction)).click
+        page.has_css?('simple_form edit_transaction')
+      end
+
+      it "shows delete form on row click" do
+        expect(subject).to have_link("Remove", href: transaction_path(last_transaction))
+      end
     end
   end
 
-  context "next page" do
+  context "deleting", js: true do
+    let!(:transactions) { create_list :transaction, 5, bank_account: account,
+      category: category }
+    let(:transaction)   { transactions.last }
+    let!(:to_click)     { transaction.amount }
+
     before do
-      click_on 'Last'
-      find("#transaction_#{last_transaction.id}",
-        text: humanized_money_with_symbol(last_transaction)).click
+      visit root_path
+      find("#transaction_#{transaction.id}",
+        text: humanized_money_with_symbol(to_click)).click
       page.has_css?('simple_form edit_transaction')
+      click_on "Remove"
     end
 
-    it "shows delete form on row click" do
-      expect(subject).to have_link("Remove", href: transaction_path(last_transaction))
+    it "removes transaction from list" do
+      expect(subject).to_not have_content(humanized_money_with_symbol(to_click))
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'pundit/rspec'
-require 'capybara-screenshot/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
