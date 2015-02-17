bf844bd58cbaa04473919ec64dbe63d80020274e
gataniel <gataniel@gmail.com>
2015-02-17 16:16:52 +0300

    Add menu options

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -3,4 +3,8 @@ module LayoutHelper
     (org.id == current_organization.id ? 'active' : '')
   end
 
+  def active_sett(sett)
+    # raise [params[:controller] == sett].inspect
+    params[:controller] == sett ? 'list-group-item active' : 'list-group-item'
+  end
 end
