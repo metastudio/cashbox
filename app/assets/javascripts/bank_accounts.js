e7adc7e8b3bbd42df9d99b2a0280876e77d1217d
gataniel <gataniel@gmail.com>
2015-03-04 10:02:40 +0300

    Add main func | ToDo for not hardcoded currecnies

--- app/assets/javascripts/bank_accounts.js
+++ app/assets/javascripts/bank_accounts.js
@@ -1,6 +1,6 @@
 $(document).on("ready page:load", function () {
-  if ($("#bank_accounts_table").length > 0) {
-    return $("#bank_accounts_table").sortable({
+  if ($("#usd_accounts").length > 0) {
+    $("#usd_accounts").sortable({
       axis:   "y",
       items:  ".item",
       delay: 200,
@@ -18,7 +18,36 @@ $(document).on("ready page:load", function () {
       update: function(e, ui) {
         return $.ajax({
           type: "PUT",
-          url: $('#bank_accounts_table').attr('data-url'),
+          url: $('#usd_accounts').attr('data-url'),
+          dataType: "json",
+          data: {
+            id: ui.item.data("item-id"),
+            position: ui.item.index()
+          }
+        });
+      }
+    });
+  }
+  if ($("#rub_accounts").length > 0) {
+    $("#rub_accounts").sortable({
+      axis:   "y",
+      items:  ".item",
+      delay: 200,
+      helper: "clone",
+      opacity: 0.5,
+      tolerance: "pointer",
+      cursor: "move",
+      sort: function(e, ui) {
+        return ui.item.addClass("active-item-shadow");
+      },
+      stop: function(e, ui) {
+        ui.item.removeClass("active-item-shadow");
+        return ui.item.children("td").effect("highlight", {}, 1000);
+      },
+      update: function(e, ui) {
+        return $.ajax({
+          type: "PUT",
+          url: $('#rub_accounts').attr('data-url'),
           dataType: "json",
           data: {
             id: ui.item.data("item-id"),
@@ -29,3 +58,4 @@ $(document).on("ready page:load", function () {
     });
   }
 });
+
