1ed12803c1edad2a015ad4e1fe1a0e3fc3e1cda4
gataniel <gataniel@gmail.com>
2015-03-23 11:41:15 +0300

    Fix some specs

--- spec/support/shared_examples/filterable.rb
+++ spec/support/shared_examples/filterable.rb
@@ -4,16 +4,15 @@ shared_examples_for "filterable object" do
   end
 
   def expect_currencies(rub, usd)
-    curr_exch = def_curr == usd.currency ? rub : usd
-    css_exch  = '.' + colorize_amount(curr_exch)
-    curr_exch = money_with_symbol Money.new(curr_exch.exchange_to(def_curr))
-
+    css_usd = '.' + colorize_amount(usd)
     css_rub = '.' + colorize_amount(rub)
-    rub = money_with_symbol rub
 
-    expect(page).to have_css(css_exch, curr_exch)
-    expect(page).to have_css(css_rub, rub)
-    expect(page).to have_css(css_usd, usd)
+    curr_exch = Money.new(rub.exchange_to('usd'))
+    css_exch = '.' + colorize_amount(curr_exch)
+
+    expect(page).to have_css(css_exch, text: money_with_symbol(curr_exch))
+    expect(page).to have_css(css_rub, text: money_with_symbol(rub))
+    expect(page).to have_css(css_usd, text: money_with_symbol(usd))
   end
 
   it "shows correct items" do
