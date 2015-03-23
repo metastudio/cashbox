dd34959c7e0e1783ea64e0bff9dcb7c5eade5579
gataniel <gataniel@gmail.com>
2015-03-23 18:48:27 +0300

    Fix spec

--- spec/features/sidebar_spec.rb
+++ spec/features/sidebar_spec.rb
@@ -1,19 +1,12 @@
 require 'spec_helper'
 
 describe 'sidebar' do
-<<<<<<< HEAD
+  include MoneyHelper
   let(:user)    { create :user }
   let(:org)     { create :organization }
   let!(:member) { create :member, organization: org, user: user, role: 'owner' }
-=======
-  include MoneyHelper
-
-  let(:user)   { create :user }
-  let(:member) { create :member, user: user }
-  let!(:org)   { member.organization }
-  let!(:ba)    { create :bank_account, organization: org }
-  let!(:account) { create :bank_account, organization: org, balance: 50000 }
->>>>>>> master
+  let(:amount)  { 5000 }
+  let!(:account){ create :bank_account, organization: org, balance: amount }
 
   before do
     sign_in user
@@ -22,53 +15,29 @@ describe 'sidebar' do
 
   subject { page }
 
-<<<<<<< HEAD
-  context "right classes for accounts" do
-    let!(:account) { create :bank_account, organization: org, balance: 50000 }
-    let!(:account_empty){ create :bank_account, organization: org, balance: 0 }
-    before do
-      visit root_path
-    end
-
-=======
   context 'accounts' do
-    let!(:ba)    { create :bank_account, organization: org, balance: amount }
-
     it_behaves_like 'colorizable amount', 'table'
   end
 
   context 'total' do
+    it_behaves_like 'colorizable amount', '#total_balance'
+
     it 'display exhanged amount' do
       within '#total_balance' do
         expect(page).to have_content(
-          money_with_symbol ba.balance.exchange_to(org.default_currency))
+          money_with_symbol account.balance.exchange_to(org.default_currency))
       end
     end
 
     it 'display exchange rate' do
       within '#total_balance' do
-        expect(page).to have_xpath("//a[contains(concat(' ', @class, ' '), ' exchange-helper ') and contains(@title, '#{Money.default_bank.get_rate(ba.currency, org.default_currency).round(4)}')]")
+        expect(page).to have_xpath("//a[contains(concat(' ', @class, ' '), ' exchange-helper ') and contains(@title, '#{Money.default_bank.get_rate(account.currency, org.default_currency).round(4)}')]")
       end
     end
 
     it 'display total balance in default currency' do
       within '#total_balance' do
-        expect(page).to have_content("Total: #{money_with_symbol account.balance.exchange_to(org.default_currency)}")
-        expect(page).to have_content money_with_symbol (ba.balance.exchange_to(org.default_currency) + ba.balance.exchange_to(org.default_currency))
-      end
-    end
-
->>>>>>> master
-    it "for positive" do
-      within '#sidebar .accounts .positive' do
-        expect(page).to have_content account.to_s
-      end
-    end
-
-    it "for empty" do
-      within '#sidebar .accounts .empty' do
-<<<<<<< HEAD
-        expect(page).to have_content account_empty.to_s
+        expect(page).to have_content money_with_symbol(account.balance.exchange_to(org.default_currency))
       end
     end
   end
@@ -151,17 +120,15 @@ describe 'sidebar' do
     describe 'members' do
       before do
         visit members_path
-=======
-        expect(page).to have_content ba.to_s
->>>>>>> master
       end
       it_behaves_like 'activatable', 'Members'
     end
 
-    context 'colorizable' do
-      let!(:ba)     { create :bank_account, organization: org, balance: amount }
-
-      it_behaves_like 'colorizable amount', '#total_balance'
+    describe 'invitations' do
+      before do
+        visit invitations_path
+      end
+      it_behaves_like 'activatable', 'Invitations'
     end
   end
 end
