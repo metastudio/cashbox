be23e43a96a035e420d3d04635d80b711887352b
gataniel <gataniel@gmail.com>
2015-03-24 19:14:45 +0300

    Fix specs

--- spec/support/matchers/have_inline_error_matcher.rb
+++ spec/support/matchers/have_inline_error_matcher.rb
@@ -4,34 +4,14 @@ RSpec::Matchers.define :have_inline_error do |expected|
     self
   end
 
-  define_method :for_field_name do |field_name|
-    @field_name = field_name
-    self
-  end
-
-  def options
-    options = if @field
-      [".form-group", text: @field]
-    elsif @field_name
-      selectors = [
-        "descendant::input[@name='#{@field_name}']",
-        "descendant::textarea[@name='#{@field_name}']",
-        "descendant::select[@name='#{@field_name}']",
-      ]
-      xpath = %Q{//*[contains(@class,'form-group') and (#{selectors.join(' or ')})]}
-      [:xpath, xpath]
-    end
-    options.join(',')
-  end
-
   match do |page|
-    within(options) do
+    within(@field) do
       page.has_content?(expected)
     end
   end
 
   match_when_negated do |page|
-    within(options) do
+    within_fieldset(@field) do
       page.has_no_content?(expected)
     end
   end
