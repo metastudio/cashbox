fb13c2131cdf66983c175fce48c11720d43a5fd1
gataniel <gataniel@gmail.com>
2015-03-25 18:21:07 +0300

    Actual money max increase | Fix exchange overflow

--- lib/russian_central_bank_safe.rb
+++ lib/russian_central_bank_safe.rb
@@ -1,4 +1,15 @@
 class RussianCentralBankSafe < Money::Bank::RussianCentralBank
+  def exchange(fractional, rate, &block)
+    ex = (fractional * BigDecimal.new(rate.to_s))
+    if block_given?
+      yield ex.to_f
+    elsif @rounding_method
+      @rounding_method.call(ex.to_f)
+    else
+      ex.to_d
+    end
+  end
+
   def update_rates(date = Date.today)
       @mutex.synchronize{
         begin
