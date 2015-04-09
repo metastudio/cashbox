6cc8ef32fd7df31b061c46fb447e478f8e451844
gataniel <gataniel@gmail.com>
2015-04-09 15:00:54 +0300

    Fix factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -115,8 +115,8 @@ FactoryGirl.define do
 
     trait :with_different_currencies do
       bank_account_id { create(:bank_account, balance: 99999, currency: 'USD').id }
-      reference_id    { |t| create(:bank_account, organization: t.organization,
-        currency: 'RUB').id }
+      reference_id    { |t| create(:bank_account, currency: 'RUB',
+        organization: BankAccount.find(t.bank_account_id).organization).id }
       from_currency   { 'USD' }
       to_currency     { 'RUB'}
       exchange_rate   0.5
