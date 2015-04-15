944af11664501f53d6ee11c1a057564e5aa99787
gataniel <gataniel@gmail.com>
2015-04-15 16:31:11 +0300

    Fix sorting specs

--- spec/support/shared_examples/sortable.rb
+++ spec/support/shared_examples/sortable.rb
@@ -1,16 +1,30 @@
 shared_examples_for "sortable object" do |sort_by, field|
   let!(:transaction)  { Timecop.travel(3.day.ago) { create :transaction,
-    :with_customer, bank_account: ba, amount: 100, comment: 'Comment',
-    category: cat  }}
+    :with_customer, bank_account: ba, amount: 100, comment: 'Comment' }}
   let!(:transaction2) { Timecop.travel(2.day.ago) { create :transaction,
-    :with_customer, bank_account: ba, amount: 200, comment: 'Comment2',
-    category: cat2 }}
+    :with_customer, bank_account: ba, amount: 200, comment: 'Comment2' }}
   let!(:transaction3) { Timecop.travel(1.day.ago) { create :transaction,
-    :with_customer, bank_account: ba, amount: 300, comment: 'Comment3',
-    category: cat3 }}
+    :with_customer, bank_account: ba, amount: 300, comment: 'Comment3' }}
   let!(:transaction4) { create :transaction, :with_customer, bank_account: ba,
-    amount: 600, comment: 'Comment4', category: cat4 }
+    amount: 600, comment: 'Comment4' }
   let(:correct_order) { [transaction, transaction2, transaction3, transaction4] }
+
+  # for correct alphabetical sort ( otherwise specs will fail occasionally )
+  def assign_associations(field)
+    if field == :category
+      transaction.update_attribute(:category_id, cat.id)
+      transaction2.update_attribute(:category_id, cat2.id)
+      transaction3.update_attribute(:category_id, cat3.id)
+      transaction4.update_attribute(:category_id, cat4.id)
+    end
+
+    if field == :customer
+      transaction.update_attribute(:customer_id, cust.id)
+      transaction2.update_attribute(:customer_id, cust2.id)
+      transaction3.update_attribute(:customer_id, cust3.id)
+      transaction4.update_attribute(:customer_id, cust4.id)
+    end
+  end
 
   def to_view(elem, field)
     case field
@@ -29,6 +43,7 @@ shared_examples_for "sortable object" do |sort_by, field|
 
   context "first sort" do
     before do
+      assign_associations(field)
       visit root_path
       within "thead" do
         click_on sort_by
