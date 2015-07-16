d07eb962cb7a9dcedd9e7288f1ff8a7e09d1c419
Yuri Skurikhin <yunixon@gmail.com>
2015-07-16 12:05:21 +0300

    customer parania fix

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -17,9 +17,19 @@ class Customer < ActiveRecord::Base
 
   validates :name, presence: true
   validates :organization, presence: true
-  validates :name, uniqueness: { scope: :organization_id }
+  validates :name, uniqueness: { scope: [:organization_id, :deleted_at] }
+
+  # gem 'paranoia' doesn't run validation callbacks on restore
+  before_restore :run_validations
 
   def to_s
     name.truncate(30)
+  end
+
+  private
+
+  def run_validations
+    self.deleted_at = nil
+    self.validate!
   end
 end
