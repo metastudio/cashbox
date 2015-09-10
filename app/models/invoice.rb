af153329064bc5295a5f5f91033180146f1cf2e1
Yuri Skurikhin <yunixon@gmail.com>
2015-09-10 13:34:20 +0300

    invoice - chack overlap dates

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,8 +5,8 @@
 #  id              :integer          not null, primary key
 #  organization_id :integer          not null
 #  customer_id     :integer          not null
-#  starts_at       :datetime
-#  ends_at         :datetime         not null
+#  starts_at       :date
+#  ends_at         :date             not null
 #  currency        :string           default("USD"), not null
 #  amount_cents    :integer          default(0), not null
 #  sent_at         :datetime
@@ -32,17 +32,31 @@ class Invoice < ActiveRecord::Base
   validates :ends_at, presence: true
   validates :amount, presence: true
   validates :currency, presence: true
+  validates :customer_name, presence: true
 
   validates :amount, numericality: { greater_than: 0,
     less_than_or_equal_to: Dictionaries.money_max }
   validates :currency, inclusion: { in: Dictionaries.currencies,
     message: "%{value} is not a valid currency" }
+  validate :check_overlap_dates_for_customer, if: 'customer_name.present?'
 
-  scope :ordered, -> { order('created_at DESC') }
+  scope :ordered,    -> { order('created_at DESC') }
+  scope :all_except, -> (invoice) { where.not(id: invoice) }
 
   after_save :set_currency
 
   private
+
+  def check_overlap_dates_for_customer
+    self.customer.invoices.all_except(self.id).where(organization: self.organization).each do |i|
+      self_start = self.starts_at ? self.starts_at : self.ends_at
+      i_start = i.starts_at ? i.starts_at : i.ends_at
+      if (self_start..self.ends_at).overlaps? (i_start..i.ends_at)
+        errors.add(:starts_at, 'Overlap date for this customer') if self.starts_at
+        errors.add(:ends_at, 'Overlap date for this customer')
+      end
+    end
+  end
 
   def set_currency
     invoice_items.each{ |i| i.update(currency: currency) }
