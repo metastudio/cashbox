412c5d03b470d59a8059c81cf59317ac2395a1cd
Yuri Skurikhin <yunixon@gmail.com>
2015-09-16 10:49:20 +0300

    fix prc

--- spec/features/remove_member_spec.rb
+++ spec/features/remove_member_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'delete transaction', js: true do
+describe 'delete member' do
   let(:user)       { create :user }
   let(:other_user) { create :user }
   let!(:org)       { create :organization }
@@ -17,7 +17,7 @@ describe 'delete transaction', js: true do
       expect(page).to_not have_content('Remove')
     end
 
-    context 'owner can remove members with admin role' do
+    context 'owner can remove members with admin role', js: true do
       let!(:member_admin) { create :member, :admin, user: other_user, organization: org }
 
       before do
@@ -28,7 +28,7 @@ describe 'delete transaction', js: true do
       it { expect(page).to_not have_content(member_admin.user_full_name) }
     end
 
-    context 'owner can remove members with user role' do
+    context 'owner can remove members with user role', js: true do
       let!(:member_user) { create :member, :user, user: other_user, organization: org }
 
       before do
@@ -55,14 +55,10 @@ describe 'delete transaction', js: true do
     context 'admin can not remove members with owner role' do
       let!(:member_owner) { create :member, :owner, user: other_user, organization: org }
 
-      before do
-        visit members_path
-      end
-
       it { expect(page).to_not have_content('Remove') }
     end
 
-    context 'admin can remove other members with admin role' do
+    context 'admin can remove other members with admin role', js: true do
       let!(:other_member_admin) { create :member, :admin, user: other_user, organization: org }
 
       before do
@@ -73,7 +69,7 @@ describe 'delete transaction', js: true do
       it { expect(page).to_not have_content(other_member_admin.user_full_name) }
     end
 
-    context 'admin can remove members with user role' do
+    context 'admin can remove members with user role', js: true do
       let!(:member_user) { create :member, :user, user: other_user, organization: org }
 
       before do
@@ -100,29 +96,17 @@ describe 'delete transaction', js: true do
     context 'user can not remove members with owner role' do
       let!(:member_owner) { create :member, :owner, user: other_user, organization: org }
 
-      before do
-        visit members_path
-      end
-
       it { expect(page).to_not have_content('Remove') }
     end
 
     context 'user can not remove members with admin role' do
       let!(:member_admin) { create :member, :admin, user: other_user, organization: org }
 
-      before do
-        visit members_path
-      end
-
       it { expect(page).to_not have_content('Remove') }
     end
 
     context 'user can not remove other members with user role' do
       let!(:other_member_user) { create :member, :user, user: other_user, organization: org }
-
-      before do
-        visit members_path
-      end
 
       it { expect(page).to_not have_content('Remove') }
     end
