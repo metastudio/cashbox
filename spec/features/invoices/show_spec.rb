7d8a3c8527445cc488b945f908226c162de6a09b
Yuri Skurikhin <yunixon@gmail.com>
2015-09-18 12:23:33 +0300

    Fix PRC

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -15,18 +15,31 @@ describe 'invoices show page', js: true do
   context 'Download as PDF' do
     let!(:invoice) { create :invoice, organization: org }
 
-    before do
-      visit invoice_path(invoice)
-      click_on 'Download as PDF'
+    context 'Download' do
+      before do
+        visit invoice_path(invoice)
+        click_on 'Download as PDF'
+      end
+
+      it { expect(page.response_headers['Content-Type']).to eq 'application/pdf' }
+      it { expect(page.response_headers['Content-Disposition']).to \
+        include("#{invoice.customer.to_s}_#{invoice.ends_at.month}_#{invoice.ends_at.year}.pdf") }
+      it { expect(page.response_headers['Content-Length']).to_not eq 0 || nil }
     end
 
-    it 'PDF content' do
-      expect(subject).to have_css('h1.uppercase', text: 'INVOICE')
-      expect(subject).to have_css('strong.uppercase', text: 'DATE')
-      expect(subject).to have_content(I18n.l(invoice.ends_at))
-      expect(subject).to have_content(invoice.currency)
-      expect(subject).to have_content('Bank account for wire transfers:')
-      expect(subject).to have_content(money_with_symbol(invoice.amount))
+    context 'Content' do
+      before do
+        visit invoice_path(invoice, format: :pdf, debug: true)
+      end
+
+      it 'PDF content' do
+        expect(subject).to have_css('h1.uppercase', text: 'INVOICE')
+        expect(subject).to have_css('strong.uppercase', text: 'DATE')
+        expect(subject).to have_content(I18n.l(invoice.ends_at))
+        expect(subject).to have_content(invoice.currency)
+        expect(subject).to have_content('Bank account for wire transfers:')
+        expect(subject).to have_content(money_with_symbol(invoice.amount))
+      end
     end
   end
 end
