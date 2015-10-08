f2a9938f36f96f454e5e9e748d36e1b94dd3ebfa
Yuri Skurikhin <yunixon@gmail.com>
2015-10-08 12:05:35 +0300

    Refactoring migration

--- db/migrate/20151007123757_update_transactions_with_deleted_customers.rb
+++ db/migrate/20151007123757_update_transactions_with_deleted_customers.rb
@@ -1,5 +1,9 @@
 class UpdateTransactionsWithDeletedCustomers < ActiveRecord::Migration
-  def change
+  def up
     Transaction.where('customer_id IN (?)', Customer.only_deleted.pluck(:id)).update_all(customer_id: nil)
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration, "Can't recover transaction's customer_id"
   end
 end
