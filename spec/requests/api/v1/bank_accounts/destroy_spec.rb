9462f57d0e7b9b021a7304a2006f79a105c4bce1
Yuri Skurikhin <yunixon@gmail.com>
2016-10-18 13:27:21 +0300

    Fix bank accounts API

--- spec/requests/api/v1/bank_accounts/destroy_spec.rb
+++ spec/requests/api/v1/bank_accounts/destroy_spec.rb
@@ -17,7 +17,9 @@ describe 'DELETE /api/organizations/#/bank_accounts/#' do
 
     it 'delete bank_account' do
       expect(response).to be_success
-      expect(response.body).to be_empty
+      expect(json).to include(
+        'id' => bank_account.id
+      )
 
       bank_account.reload
       expect(bank_account.deleted_at).to_not eq nil
@@ -29,7 +31,9 @@ describe 'DELETE /api/organizations/#/bank_accounts/#' do
 
     it 'delete bank_account' do
       expect(response).to be_success
-      expect(response.body).to be_empty
+      expect(json).to include(
+        'id' => bank_account.id
+      )
 
       bank_account.reload
       expect(bank_account.deleted_at).to_not eq nil
