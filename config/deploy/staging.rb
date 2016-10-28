850a9d3371c676031237b58c107765eb84be3ca4
AlexanderUlitin <ishotman0@gmail.com>
2016-10-28 15:15:37 +0300

    Update deploy user

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,4 +7,4 @@ set :deploy_to, '/var/www/rails/staging.cashbox.metastudiohq.com'
 set :rvm_type, :system
 set :rvm_ruby_version, 'ruby-2.3.1@cashbox'
 
-server 'metastudio.ru', user: 'developer', roles: %w{web app db worker}
+server 'metastudio.ru', user: 'deployer', roles: %w{web app db worker}
