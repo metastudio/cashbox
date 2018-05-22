f672b3623d7dfc5e457e8288fd6a77edd5e91ee8
Ildar Shaynurov <shaynurov@gmail.com>
2018-05-22 18:33:15 +0300

    feat(graphql): authenticate user

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class GraphqlController < ApplicationController
-  # include Knock::Authenticable
+  include Knock::Authenticable
 
   skip_before_action :verify_authenticity_token
   skip_before_action :authenticate_user!
