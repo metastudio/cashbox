42a145c6922f272653b638bd6c834da753da0ee6
Ildar Shaynurov <shaynurov@gmail.com>
2018-05-25 18:23:17 +0300

    feat(graphql): add mutation to update category

---
+++ app/graphql/types/category_input_type.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+Types::CategoryInputType = GraphQL::InputObjectType.define do
+  name 'CategoryInput'
+
+  argument :type, !types.String
+  argument :name, !types.String
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,8 +5,10 @@ Types::MutationType = GraphQL::ObjectType.define do
 
   field :authenticate, Types::AuthenticationType do
     description 'Sign in user with given credentials'
+
     argument :email,    !types.String
     argument :password, !types.String
+
     resolve lambda{ |_obj, args, _ctx|
       result = AuthenticateUserService.perform(args[:email], args[:password])
       if result.success?
@@ -14,6 +16,23 @@ Types::MutationType = GraphQL::ObjectType.define do
       else
         OpenStruct.new({ token: nil, errors: result.payload })
       end
+    }
+  end
+
+  field :updateCategory, Types::CategoryType do
+    description 'Update category'
+
+    argument :id,       !types.ID
+    argument :category, !Types::CategoryInputType
+
+    resolve lambda{ |_obj, args, _ctx|
+      category = Category.find(args[:id])
+      return category if category.update(args[:category].to_h)
+
+      return GraphQL::ExecutionError.new(
+        'Invalid record',
+        options: { validationErrors: category.errors }
+      )
     }
   end
 end
