66534f993e125b5961f118156c351511ee22c145
Stas Chereshkevich <chereshkevichsv@gmail.com>
2018-07-11 13:37:00 +0700

    Move logic back on server

--- app/controllers/api/v1/debtors_controller.rb
+++ app/controllers/api/v1/debtors_controller.rb
@@ -4,19 +4,46 @@ module Api::V1
   class DebtorsController < BaseOrganizationController
     def index
       debtors = []
+      total = 0
+      summ_by_currencies = []
       current_organization.invoice_debtors.each do |debtor|
-        debtor.indebtedness.each do |debt|
+        indebtedness = debtor.indebtedness
+        indebtedness.each do |debt|
+          amount = ConvertedMoneyPresenter.new(
+            debt,
+            current_organization.default_currency
+          ).present
           debtors.push({
             id: debtor.id,
             name: debtor.name,
-            amount: ConvertedMoneyPresenter.new(
-              debt,
-              current_organization.default_currency
-            ).present
+            amount: amount
+          })
+          total += amount[:total]
+        end
+        indebtedness_summ!(summ_by_currencies, indebtedness)
+      end
+      render json: {
+        debtors: debtors,
+        summ_by_currencies: summ_by_currencies,
+        total: total
+      }.to_json
+    end
+
+    private
+
+    def indebtedness_summ!(summ, indebtedness)
+      indebtedness.each do |debt|
+        name = "All customers (#{debt.currency})"
+        search = summ.select { |d| d[:name] == name }.first
+        if search.present?
+          search[:amount] += debt
+        else
+          summ.push({
+            name: name,
+            amount: debt
           })
         end
       end
-      render json: debtors.to_json
     end
   end
 end
---
+++ spec/requests/api/v1/debtors/index_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'GET /api/organizations/#/debtors' do
+  let(:path) { api_organization_debtors_path(org) }
+
+  let(:user) { create :user }
+  let(:org) { create :organization, with_user: user }
+  let!(:invoice1) { create :invoice, organization: org }
+  let!(:invoice2) { create :invoice }
+
+  context 'unauthenticated' do
+    it { get(path) && expect(response).to(be_unauthorized) }
+  end
+
+  context 'authenticated' do
+    before { get path, headers: auth_header(user) }
+
+    it 'returns debtors of current organization' do
+      expect(json['debtors'].size).to eq 1
+
+      expect(json['debtors'][0]).to include(
+        'id'    => invoice1.customer.id,
+        'name'  => invoice1.customer.to_s,
+      )
+    end
+
+    it 'return total for all debtors in default currency' do
+      expect(json['total']).not_to be_blank
+    end
+
+    it 'return summs by currencies for all debtors' do
+      expect(json['summ_by_currencies'][0]).to include(
+        'amount' => JSON.parse(invoice1.amount.to_json)
+      )
+    end
+  end
+
+  context 'authenticated as wrong user' do
+    let!(:wrong_user) { create :user }
+
+    before { get path, headers: auth_header(wrong_user) }
+
+    it 'returns error' do
+      expect(response).to be_not_found # we can't find organization with given id for current user
+    end
+  end
+end
