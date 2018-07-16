4cc1232583d326f84cc2d6b2fd2b4a18914a6915
Ildar Shaynurov <shaynurov@gmail.com>
2018-07-16 16:05:57 +0300

    chore: update ruby version for deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,6 +5,6 @@ set :rails_env, :staging
 set :deploy_to, '/var/www/rails/staging.cashbox.metastudiohq.com'
 
 set :rvm_type, :system
-set :rvm_ruby_version, 'ruby-2.3.1@cashbox'
+set :rvm_ruby_version, 'ruby-2.5.0@cashbox'
 
 server 'metastudio.ru', user: 'deployer', roles: %w{web app db worker}
