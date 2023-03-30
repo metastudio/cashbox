fa89bf8cde02dafd9629f832797fac69b71fe295
Alexander Ulitin <ishotman0@gmail.com>
2023-03-30 15:53:41 +0300

    Fix loading latest rates

--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -24,14 +24,13 @@ class ExchangeRate < ApplicationRecord
           last_db_rates = ExchangeRate.last
           if last_db_rates.present?
             Rails.logger.warn('Couldn\'t update rates from Bank, updating from DB')
-            [Hash[last_db_rates.rates.rates.map { |k, v| [k, v.to_f] }], last_db_rates.updated_from_bank_at]
+            [Hash[last_db_rates.rates.map { |k, v| [k, v.to_f] }], last_db_rates.updated_from_bank_at]
           else
             Rails.logger.warn('Couldn\'t update rates from Bank or DB, updating from yml file')
             rates = YAML.load_file(Rails.root.join('db', 'seeds', 'rates.yml'))
             [rates[:rates], Time.zone.parse(rates[:updated_from_bank_at])]
           end
         end
-
       ExchangeRate.create!(
         rates:                rates,
         updated_from_bank_at: rates_updated_at
