a8f610817f7059d73168ac8e13e33852bee96f6d
Alexander Ulitin <ishotman0@gmail.com>
2023-08-17 13:04:41 +0200

    Add RSD to currencies list

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,6 @@ set :tmp_dir, '/tmp/cashbox_production'
 set :deploy_to, '/home/deployer/apps/cashbox.metastudiohq.com'
 
 set :rvm_type, :system
-set :rvm_ruby_version, 'ruby-2.5.0@cashbox-production'
+set :rvm_ruby_version, 'ruby-2.5.3@cashbox-production'
 
 server 'cashbox.metastudiohq.com', user: 'deployer', roles: %w{web app db worker}
