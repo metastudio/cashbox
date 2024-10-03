2973cf879ac18d4eed9f94eefef25c049b87a7fc
Alexander Ulitin <ishotman0@gmail.com>
2024-10-03 19:38:30 +0200

    Fix failed specs on passing integed into Money helper

--- spec/support/shared_examples/transaction_outcome_shared.rb
+++ spec/support/shared_examples/transaction_outcome_shared.rb
@@ -4,7 +4,7 @@ RSpec.shared_examples "outcome transaction" do
   describe "attributes" do
     it { expect(out.amount_cents).to eq (transfer.amount_cents + transfer.comission_cents) * (-1) }
     it { expect(out.comment).to eq (transfer.comment.to_s +
-      "\nComission: " + money_with_symbol(transfer.comission)) +
+      "\nComission: " + money_with_symbol(transfer.money_comission)) +
       (transfer.currency_mismatch? ? "\nRate: " + transfer.exchange_rate.to_s : '') }
     it { expect(out.bank_account_id).to eq transfer.bank_account_id }
     it { expect(out.category_id).to eq Category.find_by(
